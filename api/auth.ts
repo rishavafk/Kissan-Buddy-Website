125bf62c91e97c5ea9bee9162c4ff864c47b82f5
rishavafk <shahrishav7777@gmail.com>
2026-01-05 15:20:42 +0530

    Completely remove api/auth.ts to fix build errors

--- api/auth.ts
+++
@@ -1,5 +0,0 @@
-// This file is a placeholder to overwrite a ghost file causing build errors.
-// It should be safe to delete later.
-export default (req: any, res: any) => {
-    res.status(200).json({ message: "Diagnostics placeholder" });
-};
