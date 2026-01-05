1366a5722ddf4b83adb5bee7dd6b0a8a65a08ee8
rishavafk <shahrishav7777@gmail.com>
2026-01-05 15:11:12 +0530

    Overwrite ghost api/auth.ts file

---
+++ api/auth.ts
@@ -0,0 +1,5 @@
+// This file is a placeholder to overwrite a ghost file causing build errors.
+// It should be safe to delete later.
+export default (req: any, res: any) => {
+    res.status(200).json({ message: "Diagnostics placeholder" });
+};
